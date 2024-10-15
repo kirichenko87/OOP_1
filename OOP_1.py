a112a9ac995e38065114da70288e110767d23c81
kirichenko87 <kappa134rus@gmail.com>
2024-10-15 21:36:18 +0300

    Создал класс Читатель

--- OOP_1.py
+++ OOP_1.py
@@ -20,3 +20,32 @@ class Book():
 
     def __str__(self):
         return(self.name_book, self.autor, self.publication_year)
+        
+
+        
+
+class Reader():
+    def __init__(self, 
+                 name: str, 
+                 id_ticket: int, 
+                 list_book: list,
+                 ):
+        
+        self.name = name
+        self.id_ticket = id_ticket
+        self.list_book = list_book
+        
+    
+    def add_book_list_book(self, new_book):
+        self.list_book.append(new_book)
+        
+        
+    def back_book_library(self,del_book):
+        if len(self.list_book) > 0:
+            result = "Книга не найдена"
+            for book in self.list_book:
+                if book == del_book:
+                    self.list_book.remove(del_book)
+                    result = "Книга успешно возвращена"
+        return result           
+
